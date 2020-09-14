b7d6731d1251eca9cae2ca67f2eb589eb25a0999
SpacePotato <anh.nguyen0909@knorex.com>
2020-09-14 16:36:08 +0700

    [WIP] support email subscription

--- src/routes/UserRoutes.js
+++ src/routes/UserRoutes.js
@@ -13,6 +13,7 @@ let {isAdmin} = require('../middlewares/authorization');
 //router.get('/', passport.authenticate('jwt', {session: false}), isAdmin, Controller.view.get);
 router.get('/', Controller.view.get);
 router.post('/email',Controller.send_email.post);
+router.put('/subscription');
 router.get('/:username', Controller.view_one.get);
 router.put('/:username/avatar',Controller.update_avatar_url.put);
 router.put('/:username/fullname',Controller.update_fullname.put);
