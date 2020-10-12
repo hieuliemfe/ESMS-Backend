6daf96aa58d3250a7e6048a926cb3831af8484a8
Duke40199 <binguyentuananh@gmail.com>
2020-10-12 13:44:13 +0700

    [add] -> /roles api

---
+++ src/controllers/RoleController.js
@@ -0,0 +1,25 @@
+'use strict';
+import { query } from "express-validator";
+import models from '../db/models/index';
+import status from 'http-status';
+import url from 'url';
+
+export default {
+
+    view: {
+        async get(req, res, next) {
+            
+            try {
+                const roles = await models.Role.findAll({
+                });
+                res.status(status.OK)
+                    .send({
+                        status: true,
+                        message: roles,
+                    });
+            } catch (error) {
+                next(error);
+            }
+        }
+    },
+};
