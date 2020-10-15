fb14d651d9303f8cef0dec186db3e4e2016ad990
Duke40199 <binguyentuananh@gmail.com>
2020-10-15 12:48:58 +0700

    [add] -> shift model for counter & employee relation

--- src/controllers/QueueController.js
+++ src/controllers/QueueController.js
@@ -3,34 +3,58 @@
 import { query } from "express-validator";
 import models from '../db/models/index';
 import status from 'http-status';
-
+import { Op } from 'sequelize';
+import jwt from 'jsonwebtoken';
+import counterCategory from "../db/models/counterCategory";
 export default {
 
     view: {
         async get(req, res, next) {
-
-            try {
-                const queues = await models.Queue.findAll({
-                    include: [
-                        {
-                            model: models.Category,
-                            as: "Category"
+            const token = req.headers.authorization.replace('Bearer ', '')
+            const tokenDecoded = jwt.decode(token)
+            await models.Shift.findOne({
+                where: {
+                    employeeId: tokenDecoded.employeeId
+                }
+            }).then(shift => {
+                console.log(`COUNTERID : ${shift.counterId}`)
+                models.CounterCategory.findAll({
+                    where: {
+                        counter_id: shift.counterId
+                    }
+                }).then(counterCategories => {
+                    let categoryIds = [];
+                    counterCategories.forEach(counterCategory => {
+                        categoryIds.push(counterCategory.categoryId)
+                    })
+                    console.log(`=====CounterCategory:${counterCategory.categoryId}`)
+                    models.Queue.findAll({
+                        include: [
+                            {
+                                model: models.Category,
+                                as: "Category"
+                            },
+                            {
+                                model: models.Counter,
+                                as: "Counter"
+                            }
+                        ],
+                        where: {
+                            [Op.and]: [
+                                { categoryId: categoryIds },
+                                { counter_id: null }
+                            ]
                         },
-                        {
-                            model: models.Counter,
-                            as: "Counter"
-                        }
-                    ],
-                    attributes: ["id", "number", "createdAt", "updatedAt"]
-                });
-                res.status(status.OK)
-                    .send({
-                        status: true,
-                        message: queues,
-                    });
-            } catch (error) {
-                next(error);
-            }
+                        attributes: ["id", "number", "createdAt", "updatedAt"]
+                    }).then(queues => {
+                        res.status(status.OK)
+                            .send({
+                                status: true,
+                                message: queues,
+                            });
+                    })
+                })
+            })
         }
     },
 
@@ -117,4 +141,5 @@ export default {
             }
         }
     },
-};
+}
+
