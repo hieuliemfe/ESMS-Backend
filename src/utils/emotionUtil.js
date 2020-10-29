435eab14ae71fccdb5148ab779b6ae12bd8ebb41
Tuan Anh Nguyen <binguyentuananh@gmail.com>
2020-10-29 10:41:22 +0700

    [update] calculate stress level in sumup shift api

--- src/utils/emotionUtil.js
+++ src/utils/emotionUtil.js
@@ -16,17 +16,55 @@ export const calculateShiftEmotionLevel = (shiftSessions) => {
       }
     });
   });
-  // console.log(`=============positive: ${positiveScoreSum}`)
-  // console.log(`=============negative: ${negativeScoreSum}`)
+
   const totalScoreSum = positiveScoreSum + negativeScoreSum;
   const positivePercent = (positiveScoreSum / totalScoreSum) * 100;
   const negativePercent = (negativeScoreSum / totalScoreSum) * 100;
-  // console.log(`=============positive: ${positivePercent}`)
-  // console.log(`=============negative: ${negativePercent}`)
   const difference = positivePercent - negativePercent;
-  // console.log(`=============difference: ${difference}`)
   //2 / (1 +exp(-0.05*x)) -1
   const shiftEmotionLevel = (2 / (1 + Math.exp(-0.05 * difference))) - 1;
+  // console.log(`=============positive: ${positiveScoreSum}`)
+  // console.log(`=============negative: ${negativeScoreSum}`)
+  // console.log(`=============positive: ${positivePercent}`)
+  // console.log(`=============negative: ${negativePercent}`)
+  // console.log(`=============difference: ${difference}`)
   // console.log(`=============shiftEmo: ${shiftEmotionLevel}`)
   return shiftEmotionLevel;
+}
+
+export const calculateStressLevel = (shiftSessions) => {
+  let positiveDurationSum = 0, negativeDurationSum = 0, neutralDurationSum = 0;
+
+  shiftSessions.forEach(session => {
+    positiveDurationSum += JSON.parse(session.info).positive_emotions_duration;
+    negativeDurationSum += JSON.parse(session.info).negative_emotions_duration;
+    neutralDurationSum += JSON.parse(session.info).neutral_emotions_duration;
+  });
+
+  const totalDuration = positiveDurationSum + negativeDurationSum + neutralDurationSum;
+  const negativePercent = (negativeDurationSum / totalDuration) * 100;
+  // 2 / (1 +exp(-0.06*x)) -1
+  const stressLevel = (2 / (1 + Math.exp(-0.06 * negativePercent))) - 1;
+
+  // console.log(`=============positive: ${positiveDurationSum}`)
+  // console.log(`=============negative: ${negativeDurationSum}`)
+  // console.log(`=============neutral: ${neutralDurationSum}`)
+  console.log(`=============stressLevel: ${stressLevel}`)
+  return stressLevel;
+}
+
+export const getEmotionDurations = (session) => {
+  let emotionDurations = JSON.parse(session.info).emotions_duration;
+  let result = {
+    "angry": emotionDurations[0],
+    "disgusted": emotionDurations[1],
+    "fearful": emotionDurations[2],
+    "happy": emotionDurations[3],
+    "neutral": emotionDurations[4],
+    "sad": emotionDurations[5],
+    "suprised": emotionDurations[6],
+    "noFaceDetected": emotionDurations[7],
+  }
+  console.log(`=========result:${result}`)
+  return result;
 }
