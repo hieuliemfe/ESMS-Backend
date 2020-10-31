2298bf322db5a56da0b72930283338581d816c7e
Duke40199 <binguyentuananh@gmail.com>
2020-10-31 13:45:32 +0700

    rebase to master

--- src/utils/emotionUtil.js
+++ src/utils/emotionUtil.js
@@ -64,3 +64,29 @@ export const getEmotionDurations = (session) => {
     return result;
   }
 }
+
+// export const calculateShiftEmotionLevel_OLD = (shiftSessions) => {
+//   let positiveScoreSum = 0, negativeScoreSum = 0;
+//   shiftSessions.forEach(shiftSession => {
+//     const sessions = shiftSession.Session;
+//     sessions.forEach(session => {
+//       const emotionLevel = JSON.parse(session.info).emotion_level;
+//       //if the emotion level is negative (-1<=level<0)
+//       if (emotionLevel >= -1 && emotionLevel < 0) {
+//         negativeScoreSum += ((2 / (1 + Math.exp(-10 * emotionLevel))) - 1) * emotionLevel;
+//       }
+//       //if the emotion level is positive (0<=level<=1)
+//       if (emotionLevel >= 0 && emotionLevel <= 1) {
+//         positiveScoreSum += ((2 / (1 + Math.exp(-3.7 * emotionLevel))) - 1) * emotionLevel;
+//       }
+//     });
+//   });
+
+//   const totalScoreSum = positiveScoreSum + negativeScoreSum;
+//   const positivePercent = (positiveScoreSum / totalScoreSum) * 100;
+//   const negativePercent = (negativeScoreSum / totalScoreSum) * 100;
+//   const difference = positivePercent - negativePercent;
+//   //2 / (1 +exp(-0.05*x)) -1
+//   const shiftEmotionLevel = (2 / (1 + Math.exp(-0.05 * difference))) - 1;
+//   return shiftEmotionLevel;
+// }
