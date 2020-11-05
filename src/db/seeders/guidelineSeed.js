3167f59cf426337df03560211da73f0dfed2716b
Duke40199 <binguyentuananh@gmail.com>
2020-11-05 11:20:22 +0700

    fix stress apis & add guideline

---
+++ src/db/seeders/guidelineSeed.js
@@ -0,0 +1,24 @@
+'use strict'
+
+export default [
+  {
+    title: "Dealing with irate customers",
+    link: "https://www.youtube.com/watch?v=oQtArj5U5l4"
+
+  },
+  {
+    title: "Dealing with customer complaints",
+    link: "https://www.youtube.com/watch?v=WphIXqTp_es"
+
+  },
+  {
+    title: "Dealing with angry customers",
+    link: "https://www.youtube.com/watch?v=T20hV4ynU7o"
+
+  },
+  {
+    title: "Dealing with difficult customers",
+    link: "https://www.youtube.com/watch?v=a1nrWFCys6A"
+
+  },
+]
