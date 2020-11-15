fe6b713208a305cffabd72f6c997d61c1f60f9c1
Kudo <hieuliemfe@gmail.com>
2020-11-16 01:17:31 +0700

    change seed

--- src/db/seeders/employeeSeed.js
+++ src/db/seeders/employeeSeed.js
@@ -17,23 +17,23 @@ export default [
   {
     id: '9903c282-06ed-48fe-9607-76e7903f6b72',
     employeeCode: 'AD0002',
-    fullname: 'Nguyen Hieu Liem',
+    fullname: 'Nguyen Van Admin',
     password: 'password',
-    email: 'hieuliem@gmail.com',
+    email: 'admin@gmail.com',
     phoneNumber: '0321456789',
     isSubscribed: true,
-    avatarUrl: 'https://avatars1.githubusercontent.com/u/64410838?s=400&u=6634d5bf03b3a155b6be5e26c3db3fbdb316e597&v=4',
+    avatarUrl: 'https://storage.googleapis.com/evidence_stream/avatar4.jpg',
     roleId: 1
   },
   {
     id: 'bfad3537-875c-4bf2-bb97-41c00b912d76',
     employeeCode: 'MG0001',
-    fullname: 'Cao Thanh Thu',
+    fullname: 'Nguyen Hieu Liem',
     password: 'password',
-    email: 'thucao@gmail.com',
+    email: 'hieuliemfe@gmail.com',
     phoneNumber: '0987654321',
     isSubscribed: true,
-    avatarUrl: 'https://avatars0.githubusercontent.com/u/45757676?s=400&u=2c550d11c1df70be4bc2a9edde0bf1514acd759e&v=4',
+    avatarUrl: 'https://storage.googleapis.com/evidence_stream/avatar2.jpg',
     roleId: 2
 
   },
@@ -45,7 +45,7 @@ export default [
     email: 'nguyentuananh@gmail.com',
     phoneNumber: '0456123789',
     isSubscribed: true,
-    avatarUrl: 'https://avatars1.githubusercontent.com/u/7835384?s=460&u=78fb0bd55818493d330277e648eb57bdc67c66df&v=4',
+    avatarUrl: 'https://storage.googleapis.com/evidence_stream/avatar3.jpg',
     roleId: 3
 
   },
@@ -57,7 +57,7 @@ export default [
     phoneNumber: '0456123789',
     email: 'khangle@email.com',
     is_subscribed: true,
-    avatarUrl: 'https://firebasestorage.googleapis.com/v0/b/facefood-41e90.appspot.com/o/avatars%2Favatar_none.png?alt=media&token=99b44006-6136-4c11-8f8c-3f82d65483e6',
+    avatarUrl: 'https://storage.googleapis.com/evidence_stream/avatar1.jpg',
     roleId: 3,
     counterId: 1
 
