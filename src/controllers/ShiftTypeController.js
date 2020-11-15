bb2142b4febf6465f23407a398af7015641004d1
Novixous <anonymousvhb@gmail.com>
2020-11-15 07:09:09 +0700

    return +7 time for shift

--- src/controllers/ShiftTypeController.js
+++ src/controllers/ShiftTypeController.js
@@ -12,6 +12,20 @@ export default {
             try {
                 const shiftTypes = await models.ShiftType.findAll({
                 });
+                for (let i = 0; i < shiftTypes.length; i++) {
+                    var shiftStart = shiftTypes[i].getDataValue('shiftStart')
+                    var shiftEnd = shiftTypes[i].getDataValue('shiftEnd')
+                    shiftStart = new Date('2020-02-02T' + shiftStart + '.000Z').toLocaleTimeString("en-US", {
+                        hour12: false,
+                        timeZone: "Asia/Ho_Chi_Minh"
+                    })
+                    shiftEnd = new Date('2020-01-01T' + shiftEnd + '.000Z').toLocaleTimeString("en-US", {
+                        hour12: false,
+                        timeZone: "Asia/Ho_Chi_Minh"
+                    })
+                    shiftTypes[i].setDataValue('shiftStart', shiftStart)
+                    shiftTypes[i].setDataValue('shiftEnd', shiftEnd)
+                }
                 res.status(status.OK)
                     .send({
                         status: true,
