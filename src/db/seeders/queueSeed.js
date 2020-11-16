fabb40eeef6d94498a09342d77afc615e8b40000
Novixous <anonymousvhb@gmail.com>
2020-11-16 22:13:10 +0700

    add queue seed for customer name

--- src/db/seeders/queueSeed.js
+++ src/db/seeders/queueSeed.js
@@ -4,43 +4,51 @@ export default [
   {
     number: 1,
     categoryId: 2,
-
+    customerName: "Nguyen Van A",
   },
   {
     number: 2,
     categoryId: 2,
+    customerName: "Le Van B",
   },
   {
     number: 3,
     categoryId: 2,
-
+    customerName: "Ho Thi C",
   },
   {
     number: 4,
     categoryId: 4,
+    customerName: "Lam Van D",
   },
   {
     number: 5,
     categoryId: 4,
+    customerName: "Nguyen Thi E",
   },
   {
     number: 6,
     categoryId: 4,
+    customerName: "Truong Van F",
   },
   {
     number: 7,
     categoryId: 2,
+    customerName: "Nguyen Ba G",
   },
   {
     number: 8,
     categoryId: 4,
+    customerName: "Ly Gia H",
   },
   {
     number: 9,
     categoryId: 4,
+    customerName: "Nguyen Thi P",
   },
   {
     number: 10,
     categoryId: 4,
+    customerName: "Nguyen Phuc V",
   },
 ]
