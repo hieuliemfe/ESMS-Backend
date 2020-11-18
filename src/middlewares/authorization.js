9b5036410a443dad9b9de354e0835a1f0d2b8f6b
Novixous <anonymousvhb@gmail.com>
2020-11-19 02:01:07 +0700

    add isAdminOrmanager

--- src/middlewares/authorization.js
+++ src/middlewares/authorization.js
@@ -43,6 +43,26 @@ export const isManager = (req, res, next) => {
   });
 }
 
+export const isManagerOrAdmin = (req, res, next) => {
+  const token = req.headers.authorization.replace('Bearer ', '')
+  const tokenDecoded = jwt.decode(token)
+
+  models.Employee.findOne({
+    attributes: ['roleId', 'isDeleted'],
+    where: { id: tokenDecoded.employeeId }
+  }).then(employee => {
+    if (employee) {
+      if (employee.isDeleted) throw new DefaultError(status.FORBIDDEN, 'Account is blocked');
+      if (tokenDecoded.roleName !== 'Manager' && tokenDecoded.roleName !== 'Admin') throw new DefaultError(status.FORBIDDEN, 'Error Forbidden');
+      next(null, employee);
+    } else {
+      throw new DefaultError(status.UNAUTHORIZED, 'You are not authorized to perform this action!');
+    }
+  }).catch(err => {
+    next(err)
+  });
+}
+
 export const isBankTeller = (req, res, next) => {
   const token = req.headers.authorization.replace('Bearer ', '')
   const tokenDecoded = jwt.decode(token)
